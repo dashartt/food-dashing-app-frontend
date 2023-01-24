ff729ca8145f6a111b3cdb0607c8a6194ea65a41
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-01-24 09:42:06 -0300

    refactor: :recycle: add autoConnect to false in socket-client

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,4 +2,6 @@ import socketio from "socket.io-client";
 
 const SOCKET_URL = "https://macacoloucopizzaria-backend.vercel.app";
 
-export const socket = socketio(SOCKET_URL);
+export const socket = socketio(SOCKET_URL, {
+  autoConnect: false,
+});
