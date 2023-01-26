1f39950db30fbc62bca0724fe490b8fbe985370c
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-01-26 09:49:56 -0300

    feat: :sparkles: add default Stepper component styles

--- src/components/providers/CustomChakraProvider.tsx
+++ src/components/providers/CustomChakraProvider.tsx
@@ -3,6 +3,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { PropsWithChildren } from "react";
 
+import theme from "@/styles/theme";
+
 export default function CustomChakraProvider({ children }: PropsWithChildren) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
