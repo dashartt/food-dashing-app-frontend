df98c03230b7e4092222ec8f91eba2db78d0d0f2
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-01-26 10:59:52 -0300

    feat: :sparkles: add pizzeria logo in avatar component and page icon

--- src/app/(admin)/head.tsx
+++ src/app/(admin)/head.tsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title>Macaco Louco - Ilha Solteira - App admin</title>
+      <title>Macaco Louco Pizzaria - App admin</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Macaco Louco App admin" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/static/logo.ico" />
     </>
   );
 }
--- src/app/(client)/head.tsx
+++ src/app/(client)/head.tsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title>Macaco Louco - Ilha Solteira - App</title>
+      <title>Macaco Louco Pizzaria - App</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Macaco Louco App" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/static/logo.ico" />
     </>
   );
 }
