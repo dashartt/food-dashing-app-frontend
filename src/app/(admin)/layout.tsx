1194f87a6b9db8daf912591c3553132e86b36ffc
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-02 06:52:13 -0300

    refactor: :lipstick: update background color

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head />
-      <body id="__next" className="bg-gray-100">
+      <body id="__next" className="bg-[#6691da]">
         <ProviderAIO withSession>
           <Header />
           {children}
