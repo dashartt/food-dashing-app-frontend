8b3ce5def0560d499c774859d53070751c4b360d
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-02 07:26:21 -0300

    feat: :sparkles: add more content in expired session modal

--- src/components/blocks/session-expired/SessionExpired.tsx
+++ src/components/blocks/session-expired/SessionExpired.tsx
@@ -5,11 +5,17 @@ export default function SessionExpired() {
   const router = useRouter();
 
   return (
-    <VStack className="m-4 rounded-md border border-gray-400 bg-white p-4 shadow-lg">
+    <VStack className="m-4 rounded-md border border-gray-400 bg-white p-4 shadow-lg w-fit max-w-sm mx-auto">
       <Box className="m-10 space-y-10">
-        <Text className="text-2xl">Sessão expirou</Text>
+        <Text className="text-2xl underline underline-offset-4 font-semibold">
+          Sessão expirou
+        </Text>
+        <Text className="text-xl text-justify">
+          É necessário se autenticar novamente para ter acesso ao sistema de
+          administração
+        </Text>
         <Button
-          className="w-full bg-[#1a95f3] text-white"
+          className="w-full bg-gray-800 text-white"
           onClick={() => router.push("/auth")}
         >
           Autenticar-se
