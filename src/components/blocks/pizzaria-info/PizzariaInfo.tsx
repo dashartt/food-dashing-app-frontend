49abfcbd45b22c932945582017f185ea02f26eba
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-03 01:17:54 -0300

    refactor: :lipstick: update styles

--- src/components/blocks/pizzaria-info/PizzariaInfo.tsx
+++ src/components/blocks/pizzaria-info/PizzariaInfo.tsx
@@ -1,7 +1,6 @@
 import { Avatar, Box, Heading, HStack, VStack } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { RiArrowRightSLine } from "react-icons/ri";
 
 export default function PizzariaInfo() {
   const [mounted, setMounted] = useState(false);
@@ -14,20 +13,16 @@ export default function PizzariaInfo() {
     <>
       {mounted && (
         <Link href="/pizzaria-information">
-          <HStack className="justify-between space-x-3 bg-gray-800 rounded-full">
+          <HStack className="justify-between space-x-3 rounded-full">
             <Box className="rounded-full bg-white p-1">
               <Avatar name="Pizzaria logo" size="lg" src="/static/logo.png" />
             </Box>
             <VStack className="flex items-start space-y-0">
               <HStack className="space-x-0">
-                <Heading
-                  as="h1"
-                  className="w-[12rem] truncate text-xl text-white"
-                >
+                <Heading as="h1" className="truncate text-xl w-fit">
                   Macaco Louco <br />
                   Pizzaria
                 </Heading>
-                <RiArrowRightSLine className="bg-transparent text-4xl text-white" />
               </HStack>
             </VStack>
           </HStack>
