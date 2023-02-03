06483050064243c048ce350e5743ddc1cc515ea9
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-03 07:08:06 -0300

    feat: :sparkles: add order card skeleton and show on history page

--- src/app/(client)/history/page.tsx
+++ src/app/(client)/history/page.tsx
@@ -5,50 +5,48 @@
 import { Button, SimpleGrid, Text, VStack } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
 
 import OrderCard from "@/components/cards/OrderItemCard";
+import OrderCardSkeleton from "@/components/skeletons/OrderCardSkeleton";
 import useIdentificationState from "@/store/checkout/useIdentification";
 
 import * as api from "../../../services/api";
 
 export default function History() {
-  const [mounted, setMounted] = useState(false);
   const { _id } = useIdentificationState();
 
-  const { data } = useQuery({
+  const { data, isFetched, isLoading } = useQuery({
     queryKey: ["orders"],
     queryFn: () => api.getClientOrders(_id),
   });
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
   return (
-    <>
-      {mounted && (
-        <SimpleGrid
-          columns={{ base: 1, md: 2 }}
-          spacing={10}
-          className="max-w-fit mx-auto"
-        >
-          {data && data.length > 0 ? (
-            data?.map((order) => (
-              <OrderCard key={order._id} order={order} date />
-            ))
-          ) : (
-            <VStack className="items-start space-y-4 rounded-md border border-gray-400 p-4 shadow-lg">
-              <Text className="text-xl">Nenhum pedido realizado ainda</Text>
-              <Link className="" href="/">
-                <Button className="rounded-md bg-gray-900 p-4 text-white">
-                  Ver o cardápio{" "}
-                </Button>
-              </Link>
-            </VStack>
-          )}
-        </SimpleGrid>
+    <SimpleGrid
+      columns={{ base: 1, md: 2 }}
+      spacing={10}
+      className="mx-auto max-w-fit"
+    >
+      {isLoading &&
+        Array(4)
+          .fill(0)
+          .map(() => <OrderCardSkeleton key={uuid()} />)}
+
+      {isFetched &&
+        data &&
+        data.length > 0 &&
+        data?.map((order) => <OrderCard key={order._id} order={order} date />)}
+
+      {isFetched && data && data.length === 0 && (
+        <VStack className="items-start space-y-4 rounded-md border border-gray-400 p-4 shadow-lg">
+          <Text className="text-xl">Nenhum pedido realizado ainda</Text>
+          <Link className="" href="/">
+            <Button className="rounded-md bg-gray-900 p-4 text-white">
+              Ver o cardápio
+            </Button>
+          </Link>
+        </VStack>
       )}
-    </>
+    </SimpleGrid>
   );
 }
---
+++ src/components/skeletons/OrderCardSkeleton.tsx
@@ -0,0 +1,58 @@
+import {
+  Box,
+  Card,
+  CardBody,
+  Center,
+  Skeleton,
+  SkeletonText,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+export default function OrderCardSkeleton() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <>
+      {mounted && (
+        <Box className="w-[20rem] h-fit border border-gray-300 rounded-md shadow-lg">
+          <Card variant="outline">
+            <CardBody className="space-y-4">
+              <VStack className="items-start">
+                <Center className="space-x-1">
+                  <Text className="font-bold">Pedido #</Text>
+                  <Skeleton className="w-6 h-4" />
+                </Center>
+
+                <VStack className="border border-gray-300 rounded-md p-4 w-full">
+                  <Skeleton className="w-28 h-4" />
+                  <Skeleton className="w-28 h-4" />
+                </VStack>
+
+                <Center className="space-x-1">
+                  <Text>Cliente: </Text>
+                  <Skeleton className="w-28 h-4" />
+                </Center>
+
+                <Center className="space-x-1">
+                  <Text>Endereço: </Text>
+                  <SkeletonText noOfLines={2} />
+                </Center>
+
+                <Center className="space-x-1">
+                  <Text>Pagamento: </Text>
+                  <Skeleton className="w-20 h-4" />
+                </Center>
+              </VStack>
+            </CardBody>
+          </Card>
+        </Box>
+      )}
+    </>
+  );
+}
