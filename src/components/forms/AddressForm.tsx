995faccf44b0834222abbac6474d1fb2b8e3288d
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-05 06:31:38 -0300

    feat: :sparkles: add form validation

--- src/components/forms/AddressForm.tsx
+++ src/components/forms/AddressForm.tsx
@@ -1,17 +1,54 @@
 import {
   Button,
   FormControl,
+  FormErrorMessage,
   FormLabel,
-  HStack,
   Input,
 } from "@chakra-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import useAddressesState from "src/store/checkout/useAddresses";
 import type { IAddress } from "src/types";
+import zod from "zod";
 
 import * as api from "../../services/api";
+
+const addressSchema = zod.object({
+  addressName: zod
+    .string({
+      required_error: "Preeencha o logradouro, exemplo: Rua, Passeio, etc...",
+    })
+    .min(5, {
+      message: "Preeencha o logradouro, exemplo: Rua, Passeio, etc...",
+    }),
+  addressNumber: zod
+    .string({
+      required_error: "Preencha o numero da residência",
+    })
+    .min(1, { message: "Preencha o numero da residência" }),
+  complement: zod
+    .string()
+    // .min(3, {
+    //   message: "Digite o complemento completamente, exemplo: apto 29",
+    // })
+    .optional(),
+  districtName: zod
+    .string({
+      required_error: "Preeencha o nome do bairro, exemplo: Jardim Aeroporto",
+    })
+    .min(3, {
+      message: "Preeencha o nome do bairro, exemplo: Jardim Aeroporto",
+    }),
+  referencePoint: zod
+    .string()
+    // .min(5, {
+    //   message:
+    //     "Digite a referência completamente, exemplo: Perto da Arno Hausser",
+    // })
+    .optional(),
+});
 
 type Props = {
   addressId?: string;
@@ -23,12 +60,19 @@ export default function AddressForm({ addressId = "" }: Props) {
   const { addAddress, setAddress, getAddress, updateAddress } =
     useAddressesState();
 
-  const initialAddress = getAddress(addressId);
-  const { handleSubmit, register } = useForm<IAddress>({
-    ...(initialAddress && { values: { ...initialAddress } }),
+  const initialFormValue = getAddress(addressId);
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    formState: { errors },
+  } = useForm<IAddress>({
+    ...(initialFormValue && { values: { ...initialFormValue } }),
+    resolver: zodResolver(addressSchema),
   });
 
   const onSubmit = async (adressDTO: IAddress) => {
+    console.log(adressDTO);
     if (addressId !== "") {
       api
         .updateAddress(addressId, {
@@ -73,60 +117,80 @@ export default function AddressForm({ addressId = "" }: Props) {
           onSubmit={handleSubmit(onSubmit)}
           className="w-full space-y-4 p-4"
         >
-          <FormControl className="-space-y-2">
+          <FormControl isInvalid={!!errors.addressName}>
             <FormLabel htmlFor="addressName">Logradouro</FormLabel>
             <Input
-              className="bg-gray-100 placeholder:text-gray-600 border border-gray-400"
+              className="border border-gray-400 bg-gray-100 placeholder:text-gray-600"
               {...register("addressName")}
               id="addressName"
               placeholder="ex: Rua Araras"
             />
+
+            <FormErrorMessage>{errors.addressName?.message}</FormErrorMessage>
           </FormControl>
 
-          <HStack>
-            <FormControl className="-space-y-2">
-              <FormLabel htmlFor="addressNumber">
-                Numero da residência
-              </FormLabel>
-              <Input
-                className="bg-gray-100 placeholder:text-gray-600 border border-gray-400"
-                {...register("addressNumber")}
-                id="addressNumber"
-                placeholder="ex: 290"
-              />
-            </FormControl>
+          <FormControl isInvalid={!!errors.addressNumber}>
+            <FormLabel htmlFor="addressNumber">Numero da residência</FormLabel>
+            <Input
+              className="border border-gray-400 bg-gray-100 placeholder:text-gray-600"
+              {...register("addressNumber")}
+              id="addressNumber"
+              placeholder="ex: 290"
+            />
 
-            <FormControl className="-space-y-2">
-              <FormLabel htmlFor="complement">Complemento (opcional)</FormLabel>
-              <Input
-                className="bg-gray-100 placeholder:text-gray-600 border border-gray-400"
-                {...register("complement")}
-                id="complement"
-                placeholder="ex: Apto 2B"
-              />
-            </FormControl>
-          </HStack>
-          <FormControl className="-space-y-2">
+            <FormErrorMessage>{errors.addressNumber?.message}</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={!!errors.complement}>
+            <FormLabel htmlFor="complement">Complemento (opcional)</FormLabel>
+            <Input
+              className="border border-gray-400 bg-gray-100 placeholder:text-gray-600"
+              {...register("complement")}
+              id="complement"
+              placeholder="ex: Apto 2B"
+            />
+
+            <FormErrorMessage>{errors.complement?.message}</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={!!errors.districtName}>
             <FormLabel htmlFor="districtName">Bairro</FormLabel>
             <Input
-              className="bg-gray-100 placeholder:text-gray-600 border border-gray-400"
+              className="border border-gray-400 bg-gray-100 placeholder:text-gray-600"
               {...register("districtName")}
               id="districtName"
               placeholder="ex: Jardim Aeroporto"
             />
+
+            <FormErrorMessage>{errors.districtName?.message}</FormErrorMessage>
           </FormControl>
 
-          <FormControl className="-space-y-2">
+          <FormControl isInvalid={!!errors.referencePoint}>
             <FormLabel htmlFor="referencePoint">
               Ponto de referência (opcional)
             </FormLabel>
 
             <Input
-              className="bg-gray-100 placeholder:text-gray-600 border border-gray-400"
-              {...register("referencePoint")}
+              className="border border-gray-400 bg-gray-100 placeholder:text-gray-600"
+              {...register("referencePoint", {
+                validate: {
+                  required: (value) => {
+                    if (
+                      value.length > 0 &&
+                      getValues("referencePoint").length > 0
+                    )
+                      return true;
+                    return false;
+                  },
+                },
+              })}
               id="referencePoint"
               placeholder="ex: Perto de, próximo de, ..."
             />
+
+            <FormErrorMessage>
+              {errors.referencePoint?.message}
+            </FormErrorMessage>
           </FormControl>
 
           <Button
