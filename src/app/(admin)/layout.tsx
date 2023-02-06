056f21315c7c674169196cb8f7b1b1d47dd6ee6e
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-06 12:07:38 -0300

    fix: :adhesive_bandage: update import path

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -2,17 +2,18 @@ import "../../styles/global.css";
 
 import type { PropsWithChildren } from "react";
 import Header from "src/components/blocks/header/AdminHeader";
-import ProviderAIO from "src/components/providers/ProviderAIO";
+
+import AllInOneProvider from "@/components/providers/AllInOneProvider";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head />
       <body id="__next" className="bg-[#6691da]">
-        <ProviderAIO withSession>
+        <AllInOneProvider withSession>
           <Header />
           {children}
-        </ProviderAIO>
+        </AllInOneProvider>
       </body>
     </html>
   );
--- src/app/(auth)/auth/layout.tsx
+++ src/app/(auth)/auth/layout.tsx
@@ -1,14 +1,14 @@
 import "../../../styles/global.css";
 
 import type { PropsWithChildren } from "react";
-import ProviderAIO from "src/components/providers/ProviderAIO";
+import AllInOneProvider from "src/components/providers/AllInOneProvider";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head />
-      <body id="__next" className="bg-[#6691da]">
-        <ProviderAIO>{children}</ProviderAIO>
+      <body id="__next" className="bg-[#637eab]">
+        <AllInOneProvider>{children}</AllInOneProvider>
       </body>
     </html>
   );
