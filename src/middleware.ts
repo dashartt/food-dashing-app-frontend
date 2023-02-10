820388b998ba39924f4c8e44aa17e4b3b58ec60a
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-10 02:05:13 -0300

    fix: :adhesive_bandage: unnecessary token validation request

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,6 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-import * as api from "./services/api";
-
 export async function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const path = request.nextUrl.pathname;
@@ -11,12 +9,7 @@ export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value || "";
 
   if (token === "") {
-    const data = await api.verifyAuth({ token });
-    const hasAuth = data?.isSuccess;
-
-    if (!hasAuth) {
-      return NextResponse.redirect(new URL("/auth", request.url));
-    }
+    return NextResponse.redirect(new URL("/auth", request.url));
   }
 
   return NextResponse.next({
