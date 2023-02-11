60195157055205370d3a9c01506461dfb2f8bbc2
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-11 10:56:16 -0300

    feat: :sparkles: add text information

--- src/app/(client)/history/page.tsx
+++ src/app/(client)/history/page.tsx
@@ -2,7 +2,15 @@
 
 "use client";
 
-import { Button, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertIcon,
+  Box,
+  Button,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -27,31 +35,39 @@ export default function History() {
   useEffect(() => router.refresh(), []);
 
   return (
-    <SimpleGrid
-      columns={{ base: 1, md: 2 }}
-      spacing={10}
-      className="mx-auto max-w-fit"
-    >
-      {isLoading &&
-        Array(4)
-          .fill(0)
-          .map(() => <OrderCardSkeleton key={uuid()} />)}
+    <Box>
+      <Alert variant="blank" className="text-xl mb-8 w-fit">
+        <AlertIcon className="self-start mt-1" />
+        Clique em algum pedido para ir para a página de detalhes
+      </Alert>
+      <SimpleGrid
+        columns={{ base: 1, md: 2 }}
+        spacing={10}
+        className="mx-auto max-w-fit"
+      >
+        {isLoading &&
+          Array(4)
+            .fill(0)
+            .map(() => <OrderCardSkeleton key={uuid()} />)}
 
-      {isFetched &&
-        data &&
-        data.length > 0 &&
-        data?.map((order) => <OrderCard key={order._id} order={order} date />)}
+        {isFetched &&
+          data &&
+          data.length > 0 &&
+          data?.map((order) => (
+            <OrderCard key={order._id} order={order} date />
+          ))}
 
-      {isFetched && data && data.length === 0 && (
-        <VStack className="items-start space-y-4 rounded-md border border-gray-400 p-4 shadow-lg">
-          <Text className="text-xl">Nenhum pedido realizado ainda</Text>
-          <Link className="" href="/">
-            <Button className="rounded-md bg-gray-900 p-4 text-white">
-              Ver o cardápio
-            </Button>
-          </Link>
-        </VStack>
-      )}
-    </SimpleGrid>
+        {isFetched && data && data.length === 0 && (
+          <VStack className="items-start space-y-4 rounded-md border border-gray-400 p-4 shadow-lg">
+            <Text className="text-xl">Nenhum pedido realizado ainda</Text>
+            <Link className="" href="/">
+              <Button className="rounded-md bg-gray-900 p-4 text-white">
+                Ver o cardápio
+              </Button>
+            </Link>
+          </VStack>
+        )}
+      </SimpleGrid>
+    </Box>
   );
 }
