d8e587e76dccb76bf070429b6fbdc8e0fc3c471a
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-12 13:16:07 -0300

    feat: :lipstick: update background and set shadow

--- src/components/layouts/sidebar-menu/content/ContentBesideSidebar.tsx
+++ src/components/layouts/sidebar-menu/content/ContentBesideSidebar.tsx
@@ -6,12 +6,12 @@ import Container from "@/components/helpers/Container";
 
 export default function ContentBesideSidebar({ children }: PropsWithChildren) {
   return (
-    <Box ml={{ base: 0, lg: 72 }} className="min-h-screen bg-gray-200 ">
+    <Box ml={{ base: 0, lg: 72 }} className="min-h-screen bg-gray-300">
       <PageTitleHeader isDefault isResponsive />
       <Box className="pt-5 md:pt-10">
         <Container
           canFit
-          className="mx-auto rounded-md border border-gray-300 bg-white p-4 lg:mx-10"
+          className="mx-auto rounded-md border border-gray-400 bg-white p-4 lg:mx-10 shadow-lg"
         >
           {children}
         </Container>
