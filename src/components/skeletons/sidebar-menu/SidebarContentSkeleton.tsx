94914e78fcdb1f59d785befd800d57e2e58ef53d
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-13 19:15:38 -0300

    fix: :adhesive_bandage: missing display handler on mobile

--- src/components/skeletons/sidebar-menu/SidebarContentSkeleton.tsx
+++ src/components/skeletons/sidebar-menu/SidebarContentSkeleton.tsx
@@ -5,7 +5,10 @@ import NavItemsLinkSkeleton from "./LinkNavItemSkeleton";
 
 export default function SidebarContentSkeleton() {
   return (
-    <Box className="fixed h-full w-full border-r-2 border-gray-200 bg-white md:w-72">
+    <Box
+      display={{ base: "none", lg: "block" }}
+      className="fixed h-full w-full border-r-2 border-gray-200 bg-white md:w-72"
+    >
       <Flex className="mb-4 h-20 items-center justify-between border-b-2 border-gray-300 px-4">
         <PizzeriaLogoSkeleton />
       </Flex>
