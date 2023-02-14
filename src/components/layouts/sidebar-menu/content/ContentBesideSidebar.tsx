2e9cc06e4eb857bd018fe4a9ba5a666b0bf7ecd9
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-14 04:43:14 -0300

    refactor: :lipstick: add default style if path doesn't include 'admin/orders'

--- src/components/layouts/sidebar-menu/content/ContentBesideSidebar.tsx
+++ src/components/layouts/sidebar-menu/content/ContentBesideSidebar.tsx
@@ -1,17 +1,23 @@
 import { Box } from "@chakra-ui/react";
+import { usePathname } from "next/navigation";
 import type { PropsWithChildren } from "react";
 
 import PageTitleHeader from "@/components/header/PageTitleHeader";
 import Container from "@/components/helpers/Container";
 
 export default function ContentBesideSidebar({ children }: PropsWithChildren) {
+  const path = usePathname();
+  const defaultStyles = "shadow-lg border border-gray-400 bg-white";
+  const styleHandler = () =>
+    path?.includes("admin/orders") ? "" : defaultStyles;
+
   return (
     <Box ml={{ base: 0, lg: 72 }} className="min-h-screen bg-gray-300">
       <PageTitleHeader isDefault isResponsive />
       <Box className="pt-5 md:pt-10">
         <Container
           canFit
-          className="mx-auto rounded-md border border-gray-400 bg-white p-4 lg:mx-10 shadow-lg"
+          className={`${styleHandler()} mx-auto rounded-md p-4 lg:mx-10`}
         >
           {children}
         </Container>
