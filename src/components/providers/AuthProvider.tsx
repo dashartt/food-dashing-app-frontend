721ad99f910941867eda351ab8f34909b5c1103c
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-15 08:48:51 -0300

    fix: :adhesive_bandage: if it's in /auth and has auth, redirect to previous page before accessing /auth

--- src/components/providers/AuthProvider.tsx
+++ src/components/providers/AuthProvider.tsx
@@ -5,18 +5,24 @@ import { usePathname, useRouter } from "next/navigation";
 import type { PropsWithChildren } from "react";
 import { useEffect, useState } from "react";
 
+import useSessionState from "@/store/useSession";
+
 import LayoutSidebarMenu from "../layouts/sidebar-menu";
 
 export default function AuthProvider({ children }: PropsWithChildren) {
   const [mounted, setMounted] = useState(false);
   const router = useRouter();
-  const path = usePathname() as string;
+  const path_ = usePathname() as string;
+  const { setPath, path } = useSessionState();
   const token = (getCookie("token") as string) || "";
 
   useEffect(() => {
     setMounted(true);
+    if (!path_.includes("auth")) {
+      setPath(path_);
+    }
     if (!token || token === "") router.push("/auth");
-    if (path?.includes("auth") && token) router.back();
+    if (path_?.includes("auth") && token) router.push(path);
   }, []);
 
   return (
