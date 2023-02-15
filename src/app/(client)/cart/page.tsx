b0587adbd150a3494416f5764993b14e302e3215
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-15 01:46:47 -0300

    feat: :children_crossing: add sticky on footer and update button color

--- src/app/(client)/cart/page.tsx
+++ src/app/(client)/cart/page.tsx
@@ -26,21 +26,23 @@ export default function ShoppingCart() {
       {mounted && (
         <VStack className="items-start space-y-4 md:w-96">
           {/* Cart items list ------------> */}
-          <Text className="p-4 text-xl">Produtos escolhidos do seu pedido</Text>
+          {/* <Box className="bg-white sticky top-20 z-10 w-full border-b-2 border-gray-400">
+            <Text className="p-4 text-xl">Produtos escolhidos</Text>
+          </Box> */}
           <VStack className="w-full space-y-4 px-4">
             {items?.map((itemCart) => (
               <ItemCartCard key={uuid()} itemCart={itemCart} />
             ))}
           </VStack>
 
-          <Box className="w-full bg-white p-4">
+          <Box className="w-full bg-white p-4 sticky bottom-0 z-10 border-t-2 border-gray-400">
             <HStack className="justify-between">
               <Text className="text-lg">
                 Total: R$ {formatCurrency(getTotalPrice())}
               </Text>
               <Button
                 onClick={() => router.push("/checkout")}
-                className="bg-[#1a95f3] text-white"
+                className="bg-gray-default text-white"
               >
                 Confirmar
               </Button>
