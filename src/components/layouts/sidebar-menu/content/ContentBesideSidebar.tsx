d5e021dfdf134f82fe2541e2daecfd8dbc60ddc3
Jonatas Queiroz Lima <joqlima5@gmail.com>
2023-02-17 07:40:38 -0300

    refactor: :children_crossing: layout with max heigth and overflow for its content get scrollbar

--- src/components/layouts/sidebar-menu/content/ContentBesideSidebar.tsx
+++ src/components/layouts/sidebar-menu/content/ContentBesideSidebar.tsx
@@ -5,7 +5,10 @@ import PageTitleHeader from "@/components/header/PageTitleHeader";
 
 export default function ContentBesideSidebar({ children }: PropsWithChildren) {
   return (
-    <Box ml={{ base: 0, lg: 72 }} className="min-h-screen">
+    <Box
+      ml={{ base: 0, lg: 72 }}
+      className="min-h-screen max-h-screen overflow-y-auto"
+    >
       <Box className="mx-auto max-w-fit mt-8">
         <Box className="mb-10 p-4 hidden lg:block w-full sticky top-0 z-10 bg-white">
           <PageTitleHeader />
